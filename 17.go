84632f4f16f4126b0b1321083a9bb6a5af136dfb
BossBoxing <boss_slam@hotmail.com>
2023-06-21 20:27:26 +0700

    17:Struct + Pointer

---
+++ 17.go
@@ -0,0 +1,20 @@
+// Struct + Pointer
+package main
+
+import "fmt"
+
+type bands struct {
+	name   string
+	member int
+}
+
+func updateMember(bands *bands, member int){
+	bands.member = member
+}
+
+func main() {
+	var greenDay = bands{name: "Green Day", member: 4}
+	fmt.Println("band", greenDay.name, "has member", greenDay.member, "people")
+	updateMember(&greenDay, 5) // when me joined the band, its funny
+	fmt.Println("band", greenDay.name, "has member", greenDay.member, "people")
+}
